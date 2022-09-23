7dedb52d54f48277c9b97f098e4d3499e6108073
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-09-23 16:31:29 -0300

    feat: Requisito 02 feito

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const UserSchema = (sequelize, DataTypes) => {
+  const UserTable = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    tableName: 'users',
+    underscored: true,
+    timestamps: false,
+  });
+  return UserTable;
+};
+
+module.exports = UserSchema;
